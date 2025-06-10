7ea71f635c87219ca227600449a7dd60d59c1c62
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2025-06-10 18:59:26 -0400

    removed sealed traits from Handler traits

--- modules/viteview/src/test/scala/com/axiommd/events/Handlers.scala
+++ modules/viteview/src/test/scala/com/axiommd/events/Handlers.scala
@@ -1,13 +1,15 @@
 package com.axiommd.events
 
 
-sealed trait MessageHandler :
+trait MessageHandler :
   protected def handleAnyArg(arg:Any): Unit
   def handleMessageArg(msg: MessageArg): Unit =
     handleAnyArg(msg.arg)
     
-sealed trait MessageTypedArgHandler[T] extends MessageHandler:
+trait MessageTypedArgHandler[T] extends MessageHandler:
   val f: T => Unit
   override def handleAnyArg(a:Any): Unit =
     f(a.asInstanceOf[T])
+
+//all handler implementations are declared from here on
 case class StringHandler(f: String => Unit) extends MessageTypedArgHandler[String] 
--- modules/viteview/src/test/scala/com/axiommd/events/Messages.scala
+++ modules/viteview/src/test/scala/com/axiommd/events/Messages.scala
@@ -9,6 +9,7 @@ sealed trait MessageTypedArg[T] extends MessageArg:
   override val arg: T
 
 
+//all messages are declared from here on
 case class MessageStringArg(arg: String) extends MessageTypedArg[String] 
 object MessageStringArg {
   given JsonEncoder[MessageStringArg] = DeriveJsonEncoder.gen[MessageStringArg]
