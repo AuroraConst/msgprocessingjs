7e59ea0507ae4182997011c2242994a954982f70
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2025-06-10 18:06:29 -0400

    making progress

--- modules/viteview/src/test/scala/com/axiommd/commands/Message.scala
+++ modules/viteview/src/test/scala/com/axiommd/commands/Message.scala
@@ -2,16 +2,20 @@ package com.axiommd.commands
 
 
 trait MessageArg:
-  lazy val name :String = this.getClass().getSimpleName().dropRight(2)
+  lazy val name :String = this.getClass().getSimpleName()
   val arg: Any
 
 trait MessageTypedArg[T] extends MessageArg:
   override val arg: T
 
-trait MessageHandler   :
-  def handle(m:MessageArg) :Unit
+trait MessageHandler :
+  protected def handleAnyArg(arg:Any): Unit
+  def handleMessageArg(msg: MessageArg): Unit =
+    handleAnyArg(msg.arg)
     
 trait MessageTypedArgHandler[T] extends MessageHandler:
   val f: T => Unit
-  def handle(m:MessageTypedArg[T]):Unit =
-    f(m.arg)
+  override def handleAnyArg(a:Any): Unit =
+    f(a.asInstanceOf[T])
+
+
--- modules/viteview/src/test/scala/com/axiommd/commands/MessageTest.scala
+++ modules/viteview/src/test/scala/com/axiommd/commands/MessageTest.scala
@@ -5,15 +5,27 @@ import com.axiommd.testutils.*
 import typings.moment.momentStrings.s
 
 class MessageTest extends SjsTesting:
+  case class MessageStringArg(arg: String) extends MessageTypedArg[String] 
+  val instance = MessageStringArg("testArg1")
+  val name = instance.name
 
   "Message.name" should {
+
     "standardize naming like this" in {
-
-      case class MessageStringArg(arg: String) extends MessageTypedArg[String] 
-      val name = MessageStringArg("test").name
-
       name shouldBe ("MessageStringArg")
-      
+    }
+    "argument value should be accessible" in {
+      instance.arg shouldBe "testArg1"
+    }
+  }
 
+
+  "MessageTypedArgHandler" should {
+    case class MessageStringArgHandler(f: String => Unit) extends MessageTypedArgHandler[String] 
+
+    val handler = MessageStringArgHandler(arg => info(s"Handling message with arg: $arg correctly"))
+
+    "handle a message with a string argument" in {
+      handler.handleMessageArg(instance)
     }
   }
