45cd74240ade36de4350d28fdf0997ee60b57f68
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2025-12-04 13:00:12 -0500

    minor update

--- modules/messaging/src/main/scala/com/axiommd/events/Messages.scala
+++ modules/messaging/src/main/scala/com/axiommd/events/Messages.scala
@@ -2,9 +2,8 @@ package com.axiommd.events
 import zio.json.*
 import scala.CanEqual.derived
 
-sealed trait MessageName :
+trait  MessageName :
   def name: String = this.getClass().getSimpleName().stripPrefix("$")
-object MessageName 
   
 case class  MessageString(s:String) extends MessageName
 
@@ -12,6 +11,7 @@ object MessageString extends MessageName:
   given JsonCodec[MessageString] = DeriveJsonCodec.gen[MessageString]
 
 case class MessageMyData(str: String, i:Int) extends MessageName
+
 object MessageMyData extends MessageName :
   given JsonCodec[MessageMyData] = DeriveJsonCodec.gen[MessageMyData]
 
