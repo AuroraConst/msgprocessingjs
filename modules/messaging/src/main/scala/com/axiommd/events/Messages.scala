53152a22654271b1669f5604b5000a7008ed8692
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2026-09-15 22:44:06 -0400

    added comments regarding codecs and typesafety  of the messagehandler

--- modules/messaging/src/main/scala/com/axiommd/events/Messages.scala
+++ modules/messaging/src/main/scala/com/axiommd/events/Messages.scala
@@ -5,8 +5,8 @@ import zio.json.*
 sealed trait  MessageName [T]:
   def name: String = this.getClass().getSimpleName().stripSuffix("$")
   type  handlerType =  T => Unit
-  var defaultHandler:Option[handlerType] = None
-  def defaultHandler_=(dh:handlerType): Unit =
+  var defaultHandler:Option[handlerType] = None   //by default the default handler is not set
+  def defaultHandler_=(dh:handlerType): Unit =  //this is a setter for the default handler which is typesafe for the specific type of message data payload 
     defaultHandler = Some(dh)
 
 
@@ -16,9 +16,9 @@ object MessageJson :
   given JsonCodec[MessageJson] = DeriveJsonCodec.gen[MessageJson]
 
 case class  MessageString(s:String) extends MessageJson 
-object MessageString  extends MessageName[MessageString]  //the companion object ensures the corresponsing json codec is applied to the case class
+object MessageString  extends MessageName[MessageString]  //the companion object ensures the corresponsing json codec is applied to MessageString
 
 case class MessageMyData(str: String, i:Int) extends MessageJson
-object MessageMyData extends MessageName[MessageMyData] //the companion object ensures the corresponsing json codec is applied to the case class
+object MessageMyData extends MessageName[MessageMyData] //the companion object ensures the corresponsing json codec is applied to MessageMyData
 
 
