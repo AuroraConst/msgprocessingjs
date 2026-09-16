26373ab49c6f00b7e71fd56219930c7b55c92b54
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2026-09-15 23:13:50 -0400

    added comment

--- modules/messaging/src/main/scala/com/axiommd/events/Messages.scala
+++ modules/messaging/src/main/scala/com/axiommd/events/Messages.scala
@@ -3,6 +3,7 @@ import zio.json.*
 
 // Base trait for all message names with a default handler lambda for handling messages of the specific type
 // as well as a message naming convention based on the case class name
+// T represents the type of the message data payload associated with this message name
 sealed trait  MessageName [T]:
   def name: String = this.getClass().getSimpleName().stripSuffix("$")
   type  handlerType =  T => Unit
